995673adcbabda7ecec0d1fd6a9fb8a485832f49
MathiasChirivi <chirivimathias3gmail.com>
2023-11-26 17:38:02 +0100

    add comment

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
-export class CommentsComponent {
+export class CommentsComponent implements OnInit{
+  news:any;
+  constructor(private dataService: DataService){
 
+  }
+
+  ngOnInit(): void {
+    this.getNewsData()
+  }
+
+  getNewsData() {
+    this.dataService.getData().subscribe((res: any) => {
+      if (res && res.data) {
+        this.news = res.data;
+        console.log(this.news)
+      }
+    });
+  }
 }
